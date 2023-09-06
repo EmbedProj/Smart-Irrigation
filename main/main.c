9672fd649d457d5514146a893347ee7688b9d321
Sidharth Raj <sidharth.raj@landisgyr.com>
2023-09-06 12:53:41 +0530

    adcSoilMoistureCode

--- main/main.c
+++ main/main.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "Adc.h"
 
 void app_main(void)
 {
+
+	ADC_soil_moisture_init(); // ADC init
+	POST_adcSoilMoisture();              // POST ADC
+
     while (true) {
-        printf("Hello from app_main!\n");
-        sleep(1);
+
+        int val = ADC_get_data();
+        ESP_LOGI("ADC", "Soil Moisture Value = %d", val);
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+
     }
 }
---
+++ src/Adc.c
@@ -0,0 +1,54 @@
+/*
+ * Adc.c
+ *
+ *  Created on: 03-Sep-2023
+ *      Author: Sidharth Raj
+ */
+#include <driver/adc.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+
+#define MIN_ADC_VALUE    0
+#define MAX_ADC_VALUE    3227
+
+static bool adcValueinRange = false;
+
+void ADC_soil_moisture_init(void)
+{
+	ESP_LOGI("ADC", "Soil Moisture Initilization Started");
+	ESP_LOGI("ADC", "WIDTH BIT 12");
+
+    adc1_config_width(ADC_WIDTH_BIT_12); // ADC 12 bit
+
+    /* Here we have used the ADC Attenuation 3
+     * it will give the maximaum value 2600mv */
+    adc1_config_channel_atten(ADC1_CHANNEL_0,ADC_ATTEN_DB_11); // ADC1 Channel 0
+}
+
+int ADC_get_data()
+{
+	int val= adc1_get_raw(ADC1_CHANNEL_0); // get data at ADC1 channel 0
+	return val;
+}
+
+static bool checkADCValueRange(int adcValue)
+{
+	// max adc value will be 3227, calculation on the basis of MaxVin=2.6 volt
+	if(adcValue>MIN_ADC_VALUE && adcValue<MAX_ADC_VALUE)
+		adcValueinRange = true ;
+
+	return adcValueinRange;
+}
+
+void POST_adcSoilMoisture()
+{
+	int val = ADC_get_data();
+	if(checkADCValueRange(val))
+	{
+		ESP_LOGI("POST Adc", "ADC Soil Moisture Value is in Range, POST Passed");
+	}
+	else
+	{
+		ESP_LOGI("POST Adc", "ADC Soil Moisture Value is Out of Range, POST Failed");
+	}
+}
